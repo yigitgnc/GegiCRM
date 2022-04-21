f9ddda822d1826f4073a11ebedcdd44d306d0dd4
YG Office PC <yyygnc@gmail.com>
2022-04-21 11:48:09 +0300

    Dal interfaces done

---
+++ GegiCRM.DAL/Abstract/IUsersAuthorizationRoleDal.cs
@@ -0,0 +1,14 @@
+﻿using GegiCRM.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GegiCRM.DAL.Abstract
+{
+    public interface IUsersAuthorizationRoleDal : IGenericDal<UsersAuthorizationRole>
+    {
+
+    }
+}
